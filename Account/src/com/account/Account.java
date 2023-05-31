b9b323b10d94c036911843d9b99f27b2d76757aa
likhithareddy2116 <likhithareddy2116@gmail.com>
2023-05-31 20:13:31 +0530

    initialcommmit

---
+++ Account/src/com/account/Account.java
@@ -0,0 +1,30 @@
+package com.account;
+
+public class Account {
+	private int accountNumber;
+	private int pin;
+	private double balance;
+
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+
+	public Account(int accountNumber, int pin, double balance) {
+		super();
+		this.accountNumber = accountNumber;
+		this.pin = pin;
+		this.balance = balance;
+	}
+
+	public int getAccountNumber() {
+		return accountNumber;
+	}
+
+	public int getPin() {
+		return pin;
+	}
+
+	public double getBalance() {
+		return balance;
+	}
+}
